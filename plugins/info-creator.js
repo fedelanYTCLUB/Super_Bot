b0e67cd8e9707422de01d6a52c5224cfa721fd17
FedelanYT <fedelanyt20@gmail.com>
2025-06-17 09:00:18 -0300

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
                 showAdAttribution: true,
                 title: 'hola ',
                 body: dev,
-                thumbnailUrl: 'https://files.catbox.moe/2a5tod.jpg',
+                thumbnailUrl: 'https://files.catbox.moe/ddv9lu.jpg',
                 sourceUrl: 'https://github.com/',
                 mediaType: 1,
                 renderLargerThumbnail: true
